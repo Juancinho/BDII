ee5149a9a5b8a612df0bf900617f4453bdac817e
alumnogreibd <alumnogreibd@greibd>
2022-03-30 10:05:04 +0200

    Interfaces

--- ProyectoBasesDatos.CopiaConCone.27/03/src/GUI/FachadaGui.java
+++ ProyectoBasesDatos.CopiaConCone.27/03/src/GUI/FachadaGui.java
@@ -4,13 +4,13 @@ package GUI;
 
 public class FachadaGui {
     proyectobasesdatos.ProyectoBasesDatos fa;
-    Principal vp;
+    Inicio vp;
      public FachadaGui(proyectobasesdatos.ProyectoBasesDatos fa){
      this.fa=fa;
-     this.vp = new Principal(fa);
+     this.vp = new Inicio();
    } 
      public void iniciaVista(){     
      vp.setVisible(true); 
-     vp.setDao(fa.getAtraccionesDAO()); 
+    // vp.setDao(fa.getAtraccionesDAO()); 
     }
 }
